10fc759435373c9900db8a1cc5de1bbb59825b37
Bob Whitmer <rjwhitmercc@gmail.com>
2020-07-07 13:42:42 -0600

    show penpals on page

--- FEpenpals/index.js
+++ FEpenpals/index.js
@@ -1,1 +1,19 @@
-console.log('ballin')
+const baseURL = "http://localhost:3000/users"
+
+fetch(baseURL)
+    .then(response => response.json())
+    .then(showPenpals)
+
+const penpals = document.querySelector('#penpals')
+const penpalList = document.querySelector('#penpal-list')
+
+function showPenpals(users){
+    users[0].penpals.forEach(penpal => {
+        const $li = document.createElement('li')
+
+        $li.textContent = penpal.name
+
+        penpalList.append($li)
+        penpals.append(penpalList)
+    })
+}
