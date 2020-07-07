6fb9c3c6ce84535064e24ed3b4013ab2035ae2c8
Bob Whitmer <rjwhitmercc@gmail.com>
2020-07-07 15:22:41 -0600

    render penpals

--- FEpenpals/index.js
+++ FEpenpals/index.js
@@ -1,19 +1,60 @@
 const baseURL = "http://localhost:3000/users"
+const penpalURL = "http://localhost:3000/penpals"
 
-fetch(baseURL)
+fetch(penpalURL)
     .then(response => response.json())
     .then(showPenpals)
 
-const penpals = document.querySelector('#penpals')
-const penpalList = document.querySelector('#penpal-list')
+const penpalCards = document.querySelector('#penpals')
+const penpalForm = document.querySelector('#penpal-form')
 
-function showPenpals(users){
-    users[0].penpals.forEach(penpal => {
-        const $li = document.createElement('li')
+penpalForm.addEventListener('submit', createPenpal)
 
-        $li.textContent = penpal.name
+function createPenpal(event){
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const name = formData.get('name')
+    const address = formData.get('address')
+    const age = formData.get('age')
 
-        penpalList.append($li)
-        penpals.append(penpalList)
+    fetch(penpalURL, {
+        method: 'POST', 
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            address: address,
+            age: age
+        })
     })
+    .then(response => response.json())
+    .then(renderPenpals(name, address, age))
+}
+
+function showPenpals(data){
+    data.forEach(penpal => {
+        renderPenpals(penpal.name, penpal.address, penpal.age)
+    })
+}
+
+function renderPenpals(name, address, age){
+    const $div = document.createElement('div')
+    const userAddress = document.createElement('p')
+    const userAge = document.createElement('p')
+
+    $div.id = "penpal-card"
+    $div.innerHTML = `<a>${capitalizeName(name)}</a>`
+    userAddress.textContent =`Address: ${address}` 
+    userAge.textContent = `Age: ${age}`
+
+    $div.append(userAddress)
+    $div.append(userAge)
+    penpalCards.append($div)
+    
+}
+
+function capitalizeName(name){
+    return name[0].toUpperCase() + name.slice(1);
 }
